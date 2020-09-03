17082a132ac687421dbbb23e79046cc8fd8a3395
Benjamin Hansen <4463738@RTS-365.dk>
2020-09-03 14:56:16 +0200

    added single Team page

---
+++ src/javascript/singleTeam.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let params = new URLSearchParams(window.location.search);
+  const category = params.get("teamId");
+  //   let key = "a5a4d5c58a0c4b919432efda878b0473";
+  let key = "0680b02955df429a92dd6bdd50d42210";
+
+  fetch(
+    `https://api.sportsdata.io/v3/csgo/scores/json/MembershipsByTeam/${category}?key=${key}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".team__name").innerHTML = data[0].TeamName;
+      document.querySelector(".sub").innerHTML = `Do you want notification from ${data[0].TeamName}`;
+      var counter = 0;
+      data.forEach(function (team) {
+        console.log(data);
+        fetch(
+          `https://api.sportsdata.io/v3/csgo/scores/json/Player/${team.PlayerId}?key=${key}`
+        )
+          .then((response) => response.json())
+          .then((player) => {
+            counter += 1;
+            document.querySelector(".teamPlayers").innerHTML += `
+            <div class="team__player">
+              <div class="team__playerNr">
+                <img src="/assets/images/placeholderPlayer.png" alt="PlaceHolder For ${player.CommonName}" class="team__player-placeholder"> 
+                <h2 class="team__player-nr">Player: ${counter}</h2>
+              </div>
+              <div class="team__playerInfo">
+                <p class="team__Player-name">Real name: <span class="team__Player-span">${player.CommonName}</span></p>
+                <p class="team__Player-matchName">In game name: <span class="team__Player-span">${player.MatchName}</span></p>
+                <p class="team__Player-birthCountry">Birth Country: <span class="team__Player-span">${player.BirthCountry}</span></p>
+              </div>
+            </div>`;
+          })
+          .catch((err) => console.log(err));
+      });
+    })
+    .catch((err) => console.log(err));
+});
