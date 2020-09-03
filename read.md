b34fe8a98ddb34c403b1999d9ae13cce94ee917c
Benjamin Hansen <4463738@RTS-365.dk>
2020-09-03 14:55:05 +0200

    Updated Notification

--- read.md
+++ read.md
@@ -1,23 +1,13 @@
 128B9B294C68167CE862F73C18F4EC3D
 
-> Appen består af ***sportsdata.io*** hvor man får info om hvilken Leagues, Holdmedlemmer, Hvem var holdmedlem, Player stats
+> Appen består af **_sportsdata.io_** hvor man får info om hvilken Leagues, Holdmedlemmer, Hvem var holdmedlem, Player stats
 
 https://sportsdata.io/developers/api-documentation/csgo#/free
 
-> Live hold nyheder
-
-
-User flow
 | User flow|
 | ------------- |
-| Find hold info      |
-| Find plyer info     |
-| Find Leagues |
-
-
-
-
-
+| Alle hold |
+| Hold plyers info |
 
 Memberships (Active)
 https://api.sportsdata.io/v3/csgo/scores/json/ActiveMemberships?key=a5a4d5c58a0c4b919432efda878b0473
--- src/app.js
+++ src/app.js
@@ -40,32 +40,89 @@ if ("serviceWorker" in navigator) {
 
 // Notifikation
 
-// Notification.requestPermission(function (status) {
-//   console.log("Notification permission status:", status);
-//   if (Notification.permission == "granted") {
-//     navigator.serviceWorker.getRegistration().then(function (reg) {
+let params = new URLSearchParams(window.location.search);
+const category = params.get("teamId");
+
+//   let key = "a5a4d5c58a0c4b919432efda878b0473";
+let key = "0680b02955df429a92dd6bdd50d42210";
+fetch(
+  `https://api.sportsdata.io/v3/csgo/scores/json/MembershipsByTeam/${category}?key=${key}`
+  // `https://api.sportsdata.io/v3/csgo/scores/json/ActiveMemberships?key=${key}`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    Notification.requestPermission(function (status) {
+      console.log("Notification permission status:", status);
+      if (Notification.permission == "granted") {
+        document
+          .querySelector(".sub")
+          .addEventListener("click", function (event) {
+            console.log(`Show notification message: "You now get notifications of ${data[0].TeamName}"`);
+            navigator.serviceWorker.getRegistration().then(function (reg) {
+              const title = data[0].TeamName;
+              var options = {
+                body: `You now get notifications of ${data[0].TeamName}`,
+                icon: "/assets/images/placeholderGroup.png",
+                vibrate: [200, 100, 200, 100, 200, 100, 400],
+                data: {
+                  dateOfArrival: Date.now(),
+                  primaryKey: 1,
+                },
+                actions: [
+                  {
+                    action: "close",
+                    title: "Close notification",
+                    icon: "/assets/images/xmark.png",
+                  },
+                ],
+              };
+
+              reg.showNotification(title, options);
+            });
+          });
+      }
+    });
+  })
+  .catch((err) => console.log(err));
+
+// let key = "a5a4d5c58a0c4b919432efda878b0473";
+// fetch(
+//   `https://api.sportsdata.io/v3/csgo/scores/json/MembershipsByTeam/100000078?key=${key}`
+//   // `https://api.sportsdata.io/v3/csgo/scores/json/ActiveMemberships?key=${key}`
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     document.getElementsByClassName("testBtn").addEventListener("click", function (event) {
+//     // self.addEventListener("push", function (event) {
+//       console.log("[Service Worker] Push Received.");
+//       console.log(
+//         `[Service Worker] Push had this data: "${event.data.text()}"`
+//       );
+
+//       const title = data[0].TeamName;
 //       var options = {
-//         body: "Here is a notification body!",
-//         icon: "assets/images/mario.png",
-//         vibrate: [100, 50, 100],
+//         body: data[0].PlayerName,
+//         icon: "/assets/images/csgo-icon.png",
+//         vibrate: [200, 100, 200, 100, 200, 100, 400],
 //         data: {
 //           dateOfArrival: Date.now(),
 //           primaryKey: 1,
 //         },
-//         actions: [
-//           {
-//             action: "explore",
-//             title: "Explore the app",
-//             icon: "images/checkmark.png",
-//           },
-//           {
-//             action: "close",
-//             title: "Close notification",
-//             icon: "images/xmark.png",
-//           },
-//         ],
+// actions: [
+//   {
+//     action: "explore",
+//     title: "Explore the app",
+//     icon: "/assets/images/checkmark.png",
+//   },
+//   {
+//     action: "close",
+//     title: "Close notification",
+//     icon: "/assets/images/xmark.png",
+//   },
+// ],
 //       };
-//       reg.showNotification("Velkommen til Gulp + PWA template", options);
+
+//       event.waitUntil(self.registration.showNotification(title, options));
 //     });
-//   }
-// });
+//   })
+//   .catch((err) => console.log(err));
