4fd6c8d036bd9867f5d66b6deaee4953b372f4ca
Benjamin Hansen <4463738@RTS-365.dk>
2020-09-04 11:27:25 +0200

    removed junk

--- src/app.js
+++ src/app.js
@@ -60,12 +60,12 @@ if (category !== null) {
             .querySelector(".sub")
             .addEventListener("click", function (event) {
               console.log(
-                `Show notification message: "You now get notifications of ${data[0].TeamName}"`
+                `Show notification message: "You are now subscribe to ${data[0].TeamName}!"`
               );
               navigator.serviceWorker.getRegistration().then(function (reg) {
                 const title = data[0].TeamName;
                 var options = {
-                  body: `You now get notifications of ${data[0].TeamName}`,
+                  body: `You are now subscribe to ${data[0].TeamName}!`,
                   icon: "/assets/images/placeholderGroup.png",
                   vibrate: [200, 100, 200, 100, 200, 100, 400],
                   data: {
@@ -89,45 +89,3 @@ if (category !== null) {
     })
     .catch((err) => console.log(err));
 }
-
-// let key = "a5a4d5c58a0c4b919432efda878b0473";
-// fetch(
-//   `https://api.sportsdata.io/v3/csgo/scores/json/MembershipsByTeam/100000078?key=${key}`
-//   // `https://api.sportsdata.io/v3/csgo/scores/json/ActiveMemberships?key=${key}`
-// )
-//   .then((response) => response.json())
-//   .then((data) => {
-//     document.getElementsByClassName("testBtn").addEventListener("click", function (event) {
-//     // self.addEventListener("push", function (event) {
-//       console.log("[Service Worker] Push Received.");
-//       console.log(
-//         `[Service Worker] Push had this data: "${event.data.text()}"`
-//       );
-
-//       const title = data[0].TeamName;
-//       var options = {
-//         body: data[0].PlayerName,
-//         icon: "/assets/images/csgo-icon.png",
-//         vibrate: [200, 100, 200, 100, 200, 100, 400],
-//         data: {
-//           dateOfArrival: Date.now(),
-//           primaryKey: 1,
-//         },
-// actions: [
-//   {
-//     action: "explore",
-//     title: "Explore the app",
-//     icon: "/assets/images/checkmark.png",
-//   },
-//   {
-//     action: "close",
-//     title: "Close notification",
-//     icon: "/assets/images/xmark.png",
-//   },
-// ],
-//       };
-
-//       event.waitUntil(self.registration.showNotification(title, options));
-//     });
-//   })
-//   .catch((err) => console.log(err));
